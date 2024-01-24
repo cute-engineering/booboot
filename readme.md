86a0837828d6337d568b0e2e233ea0dd1f54347c
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-01-24 21:12:10 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+<br/>
+<br/>
+<br/>
+<p align="center">
+    <img src="doc/logo.webp" width="200" height="200">
+</p>
+<h1 align="center">BooBoot</h1>
+<p align="center">
+    The Scawy Bootloadew
+</p>
+<br/>
+<br/>
+<br/>
---
+++ src/booboot/main.c
@@ -0,0 +1,11 @@
+#include <ce-efi/st.h>
+
+#include "base.h"
+
+efi_status efi_main(efi_handle, efi_system_table *st)
+{
+    efi_assert_success(st->boot_services->set_watchdog_timer(0, 0, 0, NULL));
+    efi_assert_success(st->con_out->reset(st->con_out, 1));
+    efi_assert_success(st->con_out->output_string(st->con_out, u"Hello, world!\r\n"));
+    return EFI_SUCCESS;
+}
