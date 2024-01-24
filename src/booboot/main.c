9a78eb99f95e0478e68ef39ec2eef9a7827f98df
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-01-24 21:25:22 +0100

    Added manifests and target files.

--- src/booboot/main.c
+++ src/booboot/main.c
@@ -2,6 +2,12 @@
 
 #include "base.h"
 
+void ce_panic(ce_cstr)
+{
+    while (1)
+        ;
+}
+
 efi_status efi_main(efi_handle, efi_system_table *st)
 {
     efi_assert_success(st->boot_services->set_watchdog_timer(0, 0, 0, NULL));
